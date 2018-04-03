600ff8b37de49dcb9cca27207aea9bb35ef5d998
James Craig Burley <james-github@burleyarch.com>
2018-04-03 03:40:14 -0400

    Fix so lazy sequences are fully realized with binding in effect

--- bash/redirection.clj
+++ bash/redirection.clj
@@ -6,16 +6,16 @@
   "Evaluate body with *in* bound to file"
   [p & body]
   `(with-open [my-in# (clojure.java.io/reader ~p)]
-     (binding [*in* my-in#] ~@body)))
+     (binding [*in* my-in#] (doall ~@body))))
 
 (defmacro >
   "Evaluate body with *out* bound to file"
   [p & body]
   `(with-open [my-out# (clojure.java.io/writer ~p)]
-     (binding [*out* my-out#] ~@body)))
+     (binding [*out* my-out#] (doall ~@body))))
 
 (defmacro >>
   "Evaluate body with *out* bound to file, appending to the end"
   [p & body]
   `(with-open [my-out# (clojure.java.io/writer ~p :append true)]
-     (binding [*out* my-out#] ~@body)))
+     (binding [*out* my-out#] (doall ~@body))))
